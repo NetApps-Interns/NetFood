d56e974398af29b73f253ff3f0084ed06a5526b8
IMOH <princeemmanuel05@gmail.com>
2022-03-07 14:22:00 +0100

    initial commit

---
+++ res/script.js
@@ -0,0 +1,21 @@
+var i = 0;
+var txt = "Enter your delivery address...";
+var speed = 50;
+
+function typeWriter() {
+	if (i < txt.length) {
+		document.getElementById("input").innerHTML += txt.charAt(i);
+		i++;
+		setTimeout(typeWriter, speed);
+	}
+}
+
+// Mobile View Link icon display
+$(".js--nav-icon").click(function () {
+	var nav = $(".main-nav");
+	var icon = $(".js--nav-icon");
+
+	nav.slideToggle(200);
+
+	// if (icon.hasClass)
+});
