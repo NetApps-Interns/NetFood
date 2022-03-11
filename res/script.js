ecbf5ac955721a3f159ed2d20ee6f42b77d8cef8
IMOH <princeemmanuel05@gmail.com>
2022-03-11 16:59:20 +0100

    feat::sparkles: Added a Log in and Sign Up page with tab view

--- res/script.js
+++ res/script.js
@@ -15,7 +15,34 @@ $(".js--nav-icon").click(function () {
 	var nav = $(".main-nav");
 	var icon = $(".js--nav-icon");
 
-	nav.slideToggle(200);
+	nav.slideToggle(300);
 
 	// if (icon.hasClass)
 });
+
+
+function openPage(pageName, elmnt) {
+	// Hide all elements with class="tabcontent" by default 
+	var i, tabcontent, tablinks;
+
+	tabcontent = document.getElementsByClassName("tabcontent");
+
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}	
+
+
+
+	// Show the specific tab content
+	document.getElementById(pageName).style.display = "block";
+
+	// 
+	// elmnt.classList.add("active");
+	elmnt.classList.add("active");
+	other.classList.remove("active");
+
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
