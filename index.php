259bf4604139371084c0ed8994876375e45a4216
IMOH <princeemmanuel05@gmail.com>
2022-03-23 16:32:08 +0100

    Refactor: :recycle: Complete overhaul of the code structure

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include_once 'CORE/config/init.php';
+
+include 'components/header.php';
+
+include 'pages/landing.php';
+
+
+include_once 'components/footer.php';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
