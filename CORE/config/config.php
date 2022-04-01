556bd6f13f7ae6a07eb7729926eaf8b25526a75e
mazzthegamer <masudhashimb@gmail.com>
2022-04-01 14:40:34 +0100

    update

--- CORE/config/config.php
+++ CORE/config/config.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
     // define('DBSERVER', 'localhost'); //DB server
     // define('DBUSERNAME', 'root'); //DB Username
     // define('DBPASSWORD', ''); //DB password
@@ -17,22 +16,3 @@
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
    //const ITEM_IMG_DIR = "/uploads/img/item/";
-=======
-    define('DBSERVER', 'localhost'); //DB server
-    define('DBUSERNAME', 'root'); //DB Username
-    define('DBPASSWORD', ''); //DB password
-    define('DBNAME', 'netfood-db'); //DB Name
-
-    // connect to MySQL DB
-    $db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
-
-    // check db connection
-    if ($db === false){
-        die("Error: Connection Error.". mysqli_connect_error());
-    }
-
-    $pdo = new PDO('mysql:host=localhost;port=3306=;dbname=netfood-db', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    const ITEM_IMG_DIR = "/uploads/img/item/";
->>>>>>> ceb8ddc90de2be4e13c9c55ed67ebddee887099b
