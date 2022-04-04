af3ffd304b06b7e92749a01e845a0bdace9b4e8e
IMOH <princeemmanuel05@gmail.com>
2022-04-04 12:33:05 +0100

    Feat:sparkles:: Added a carousel view for the Popular Meal on the Landing page

--- CORE/config/config.php
+++ CORE/config/config.php
@@ -15,4 +15,4 @@
     $pdo = new PDO('mysql:host=localhost;port=3306=;dbname=netfood-db', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    const ITEM_IMG_DIR = "/uploads/img/item/";
+    const ITEM_IMG_DIR = "uploads/img/item/";
