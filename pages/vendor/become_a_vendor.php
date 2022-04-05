9ee727f2a49680ace199b28bb905fee8588fcf8f
mazzthegamer <masudhashimb@gmail.com>
2022-04-05 11:20:30 +0100

    vendor

--- pages/vendor/become_a_vendor.php
+++
@@ -1,8 +0,0 @@
-<?php
-//include_once 'CORE/config/init.php';
-include 'components/footer.php';
-
-
-include 'components/footer.php';
-
- ?>
---
+++ vendor-folder/pages/become_a_vendor.php
@@ -0,0 +1,8 @@
+<?php
+//include_once 'CORE/config/init.php';
+include '../components/vendor_header.php';
+
+
+include '../components/vendor_footer.php';
+
+ ?>
