3ebb530709620d855829ca5e20002bbbb47d8fb6
mazzthegamer <masudhashimb@gmail.com>
2022-04-07 11:50:37 +0100

    update

--- vendor-folder/pages/v-admin.php
+++ vendor-folder/pages/v-admin.php
@@ -6,18 +6,18 @@ include '../components/vendor_header.php';
 
 
 ?>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        
+<!-- <link rel="stylesheet" href="../assets/res/style.css">
+         -->
   <section class="image-background"> 
-     <h2>Vendor dashboard </h2>
-        <div class="adds" class="col-1">     
-        <a type="button" action="submit" class="btn btn-small btn-outline-success"  href="add-item.php" value="add-item">ADD-ITEM</a>
+     <h1>Vendor dashboard </h1>
+           
+        <a type="button" action="submit" class="btn-admin"  href="add-item.php" value="add-item">ADD-ITEM</a>
         
-                <table class="table table-dark table-striped">
+                <table class="tbl-full">
         <thead>
             <tr>
-                <th scope= "col">S/N   
-                </th>
+                <th scope= "col">S/N</th>   
+                
                 <td scope= "col">food title</td>
                 <td scope= "col">food image</td>
                 <td scope= "col">ingredients</td>
@@ -42,18 +42,18 @@ include '../components/vendor_header.php';
           <td><?php echo $item['price'] ?></td>
           <td><?= date("Y F d",strtotime($item['create_date'])) ?></td>
           
-        <div class="adds">
-        <td>
-            <a type="button" action="submit" class="btn btn-small btn-outline-primary" href="edit.php?id=<?= $item['id']; ?>">EDIT</a>
-            <a type="button" action="submit" class="btn btn-small btn-outline-danger" href="delete.php?id=<?= $item['id']; ?>">DELETE</a>
-            <a type="button" action="submit" class="btn btn-small btn-outline-success" href="index.php?id=<?= $item['id']; ?>" value="preview">PREVIEW</a>
+       
+        <td colspan="2">
+            <a type="button" action="submit" class="btn-edit" href="edit.php?id=<?= $item['id']; ?>">EDIT</a>
+            <a type="button" action="submit" class="btn-delete" href="delete.php?id=<?= $item['id']; ?>">DELETE</a>
+            <a type="button" action="submit" class="btn-preview" href="index.php?id=<?= $item['id']; ?>" value="preview">PREVIEW</a>
         </td>
       <?php endforeach; ?>
     </tr>
-      </div>
+      
       </tbody>
     </table>
-        </div>
+        
 </section>
 
 <?php include '../components/vendor_footer.php'; ?>
