10635afbc4bb3623e060a34ebdc46f0d5f1aa6d2
IMOH <princeemmanuel05@gmail.com>
2022-04-27 06:44:09 +0100

    :card_file_box: Config.php now configured to connect to remote server for shared DB

--- CORE/config/config.php
+++ CORE/config/config.php
@@ -1,10 +1,35 @@
 <?php
-    define('DBSERVER', 'localhost'); //DB server
-    define('DBUSERNAME', 'root'); //DB Username
-    define('DBPASSWORD', ''); //DB password
+    
+    try {
+    ////Connection to Local Server 
+    // define('DBSERVER', 'localhost'); //DB server
+    // define('DBUSERNAME', 'root'); //DB Username
+    // define('DBPASSWORD', ''); //DB password
+    // define('DBNAME', 'netfood-db'); //DB Name
+
+    // // connect to MySQL DB
+    
+    // $db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
+
+    // // check db connection
+    // if ($db === false){
+    //     die("Error: Connection Error.". mysqli_connect_error());
+    // }
+
+    // $pdo = new PDO('mysql:host=localhost;port=3306=;dbname=netfood-db', 'root', 'tt');
+    // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    
+
+    //Connection to Remote Shared server AKA Jesse's Desktop
+    define('DBSERVER', '192.168.0.122'); //DB server
+    define('DBUSERNAME', 'remote'); //DB Username
+    define('DBPASSWORD', 'remote'); //DB password
     define('DBNAME', 'netfood-db'); //DB Name
 
     // connect to MySQL DB
+    
     $db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
 
     // check db connection
@@ -12,8 +37,13 @@
         die("Error: Connection Error.". mysqli_connect_error());
     }
 
-    $pdo = new PDO('mysql:host=localhost;port=3306=;dbname=netfood-db', 'root', '');
+    $pdo = new PDO('mysql:host=192.168.0.122;port=3306=;dbname=netfood-db', 'remote', 'remote');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch (PDOException $th) {
+        //throw $th;
+        die($th->getMessage());
+    }
 
     const ITEM_IMG_DIR = "uploads/img/item/";
     
